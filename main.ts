b8ce87b125fa00e97858daa157fd7da3aa4f8803
FUF1 <69962736+FUF1@users.noreply.github.com>
2023-03-27 17:43:53 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,18 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendValue("name", 0)
+    radio.sendNumber(0)
+    basic.showIcon(IconNames.Yes)
+    basic.pause(2000)
+    basic.clearScreen()
 })
-radio.setGroup(1)
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+    basic.showIcon(IconNames.Yes)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+radio.setGroup(4)
+radio.setTransmitPower(7)
+radio.setTransmitSerialNumber(true)
 basic.forever(function () {
 	
 })
